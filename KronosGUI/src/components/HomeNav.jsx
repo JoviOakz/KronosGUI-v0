45cc0830f184843c49d538dda5eed984b76a02b3
JoviOakz <joao.mateussdc@gmail.com>
2024-08-15 20:09:34 -0300

    feat: dropdown function update

--- KronosGUI/src/components/HomeNav.jsx
+++ KronosGUI/src/components/HomeNav.jsx
@@ -6,9 +6,15 @@ import Logo from "/Logo.jfif";
 const Navbar = () => {
     const navigate = useNavigate();
     const options = [
-        'one', 'two', 'three'
+        { value: 'shadows', label: 'Shadows of Evil' },
+        { value: 'eisendrache', label: 'Der Eisendrache' },
+        { value: 'krovi', label: 'Gorod Krovi' }
     ];
-    const defaultOption = options[0];
+    const defaultOption = 'Choose a Map';
+
+    const handleDropdownChange = (option) => {
+        navigate(`/${option.value}`);
+    };
 
     return (
         <>
@@ -20,10 +26,9 @@ const Navbar = () => {
                 <div className="flex justify-between items-center w-10/12">
                     <div className="text-white">KronosGUI</div>
                     <div>
-                        <ul className="flex gap-16">
+                        <ul className="flex gap-12">
                             <button className="hover:text-sky-700 text-white" onClick={() => navigate("/")}>Home</button>
-                            <Dropdown options={options}  value={defaultOption} placeholder="Select an option" />
-                            {/* <button className="hover:text-sky-700 text-white" onClick={() => navigate("/")}>DROPDOWN</button> */}
+                            <Dropdown options={options} onChange={handleDropdownChange} value={defaultOption} />
                             <button className="hover:text-sky-700 text-white" onClick={() => navigate("/")}>About</button>
                         </ul>
                     </div>
