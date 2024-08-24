e164a7d258dbe0c44e8055ddf709eb9ee97b803c
JoviOakz <joao.mateussdc@gmail.com>
2024-08-24 18:07:30 -0300

    fix: InformationCard updated and aplied

--- KronosGUI/src/components/InformationCard.jsx
+++ KronosGUI/src/components/InformationCard.jsx
@@ -1,20 +1,19 @@
-import SOE from "/SOE-thumb.webp";
 import Button from "./Button/Button";
 
-const InformationCard = () => {
+const InformationCard = ({children, mapName, mapImage}) => {
     return (
         <>
-            <div className="flex flex-col w-[45%] h-[80vh] rounded-3xl bg-white shadow-md shadow-slate-600">
+            <div className="flex flex-col w-[45%] h-[92vh] rounded-3xl bg-white shadow-md shadow-slate-600">
                 <div>
-                    <img className="rounded-t-3xl" src={SOE} />
+                    <img className="rounded-t-3xl" src={mapImage} />
                 </div>
 
                 <div className="flex justify-center p-4">
-                    <p className="font-serif text-[32px]">Shadows of Evil</p>
+                    <p className="font-serif text-[32px]">{mapName}</p>
                 </div>
 
                 <div className="flex flex-col h-full justify-between items-center pb-8">
-                    <p className="flex pl-10 pr-10 text-justify">Shadows of Evil is the eighteenth (chronologically third) Zombies map. It is the first Zombies map in Call of Duty: Black Ops III. The setting takes place in the fictional city of Morg City, from the 1940s, where it introduces four new characters, Nero Blackstone, Jessica Rose, Jack Vincent and Floyd Campbell as they are transported to a nightmarish realm by the Shadow Man, a supernatural being, leader of the Apothicons.</p>
+                    <p className="flex pl-10 pr-10 text-justify">{children}</p>
                     <Button onClick={() => console.log("Teste")}>Enter</Button>
                 </div>
             </div>
--- KronosGUI/src/pages/Home.jsx
+++ KronosGUI/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import PageEnveloper from "../components/PageEnveloper";
 import Background from "/Home-bg.png";
 import GSButton from "../components/GetStartedButton/Button"
 import InformationCard from "../components/InformationCard";
+import SOE from "/SOE-thumb.png";
+import DE from "/DE-thumb.webp";
+import GK from "/GK-thumb.jpg"
 
 const Home = () => {
     return (
@@ -17,9 +20,15 @@ const Home = () => {
                     </div>
 
                     <div className="flex flex-wrap justify-around gap-12 p-8">
-                        <InformationCard />
-                        <InformationCard />
-                        <InformationCard />
+                        <InformationCard mapName={"Shadows of Evil"} mapImage={SOE}>
+                            Shadows of Evil is the eighteenth (chronologically third) Zombies map. It is the first Zombies map in Black Ops 3. The setting takes place in the fictional city of Morg City, from the 1940s, where it introduces four new characters, Nero Blackstone, Jessica Rose, Jack Vincent and Floyd Campbell as they are transported to a nightmarish realm by the Shadow Man, a supernatural being, leader of the Apothicons.
+                        </InformationCard>
+                        <InformationCard mapName={"Der Eisendrache"} mapImage={DE}>
+                            Der Eisendrache (The Iron Dragon) is the third Zombies map in Black Ops 3. The Primis crew must go to this elusive castle located in the Austrian Alps, in order to retrieve Ultimis Dempsey’s soul. This castle was occupied by members of Group 935 and was dubbed “Griffin Castle” by its members, two most notable members were Dr. Edward Richtofen and Dr. Maxis. The castle was used for rocket testing purposes as well as developing the Wunder Sphere and even creating a Death Ray.
+                        </InformationCard>
+                        <InformationCard mapName={"Gorod Krovi"} mapImage={GK}>
+                            Gorod Krovi (City of Blood) is the fifth Zombies map in Black Ops 3. The four travel to a war-torn Stalingrad, in another alternate universe, during a massive battle between several Group 935 controlled Dragons and Russian Giant Robots in search for this universe's Nikolai Belinski.
+                        </InformationCard>
                     </div>
                 </div>
             </div>
