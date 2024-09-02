9034f62f8526a0f4d7e8e18b723f1e5069661e4e
JoviOakz <joao.mateussdc@gmail.com>
2024-09-02 18:47:27 -0300

    fix

--- KronosGUI/src/components/Footer.jsx
+++ KronosGUI/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "/Logo.jfif";
 
 const Footer = () => {
     return (
-        <div className="flex flex-col justify-evenly footer w-full h-[24vh] bg-[#01091C] shadow-[0_-4px_6px_-1px,0_-2px_4px_-2px] shadow-slate-900">
+        <div className="flex flex-col justify-evenly footer w-full h-[32vh] bg-[#01091C] shadow-[0_-4px_6px_-1px,0_-2px_4px_-2px] shadow-slate-800">
             <div className="flex justify-center flex-wrap gap-16 items-center">
                 <div className="w-80 h-[2px] bg-slate-200 opacity-40" />
                 <ul className="flex gap-8">
