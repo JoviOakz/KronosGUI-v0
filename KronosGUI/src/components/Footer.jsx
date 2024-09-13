d4d1de7cbadf6b21245d272c1bb111ac091292fd
JoviOakz <joao.mateussdc@gmail.com>
2024-09-13 11:29:23 -0300

    feat: new functions

--- KronosGUI/src/components/Footer.jsx
+++ KronosGUI/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Github from "/GitHub-icon.png";
 import Instagram from "/Instagram-icon.png";
 import Logo from "/Logo.jfif";
@@ -8,14 +9,14 @@ const Footer = () => {
             <div className="flex justify-center flex-wrap gap-16 items-center">
                 <div className="w-80 h-[2px] bg-slate-200 opacity-40" />
                 <ul className="flex gap-8">
-                    <img src={Github} />
-                    <img src={Instagram} />
+                    <a href="https://github.com/JoviOakz" target="_blank"><img src={Github} /></a>
+                    <a href="https://instagram.com/jovioakz_" target="_blank"><img src={Instagram} /></a>
                 </ul>
                 <div className="w-80 h-[2px] bg-slate-200 opacity-40" />
             </div>
 
             <div className="flex flex-col justify-center items-center bg-[#01091C]">
-                <img className="w-24 rounded-lg" src={Logo} />
+                <Link to="/"><img className="w-24 rounded-lg" src={Logo} /></Link>
                 <p className="text-slate-200">2024</p>
                 <div>
                     <ul className="flex gap-3 items-center">
